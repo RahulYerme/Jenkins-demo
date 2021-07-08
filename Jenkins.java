b28947bc9a58d97362e84632269a3363f429c3cc
Rahul <rahulyerme1997@gmail.com>
2021-07-08 11:37:04 +0000

    Adding a new commit for chron systax

--- Jenkins.java
+++ Jenkins.java
@@ -5,6 +5,7 @@ class Jenkins
 		System.out.println("Hello-world");
 		 System.out.println("New line for Polling");
                  System.out.println("New build");
+                  System.out.println("Adding new line for chron syntax");
 	}
 }
  
